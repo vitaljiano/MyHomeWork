2f51651ec55c23c122932ed5924ac03fd853243f
Vitalik <vitaljiano@gmail.com>
2020-10-22 10:23:54 +0300

    Finish WH7_Task3

--- src/JavaCoreBasics/HW7/Task3/Main.java
+++ src/JavaCoreBasics/HW7/Task3/Main.java
@@ -2,6 +2,20 @@ package JavaCoreBasics.HW7.Task3;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        int[][] arr = new int[5][7];
+        int a = -2;
+        int b = 75;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                arr[i][j] = (a + (int)( Math.random() * b));
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < 7; j++) {
+                System.out.print(arr[i][j]+" ");
+
+            }
+        }
     }
 }
