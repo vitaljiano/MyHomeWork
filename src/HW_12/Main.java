00711c8fee08d6adf51baf89867c4fe62bc1ac9b
Vitalik <vitaljiano@gmail.com>
2020-11-14 19:21:49 +0200

    checked HW_12

---
+++ src/HW_12/Main.java
@@ -0,0 +1,37 @@
+package HW_12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+       String str;
+        do {
+            str = scanner.nextLine().trim();
+            if(!str.equals("break")){
+                list.add(str);
+            }
+        }while (!str.equals("break"));
+
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println("");
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).matches("^s")){
+            System.out.println(list.get(i));
+            }
+        }
+        System.out.println("");
+
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i).matches("^[.....*]"))
+//            System.out.println(list.get(i));
+//        }
+    }
+}
