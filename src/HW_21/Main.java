88479cc7ec78867380589f6ae18447d15baa2ce3
Vitalik <vitaljiano@gmail.com>
2020-12-06 17:22:12 +0200

    finished HW_21

---
+++ src/HW_21/Main.java
@@ -0,0 +1,12 @@
+package HW_21;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.fieldsOfAnn();
+
+    }
+}
